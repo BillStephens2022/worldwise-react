246fcf9fa822d88d27a0f0803a60ebf7eead7dd6
BillStephens2022 <two4onebill@yahoo.com>
2024-07-22 13:48:55 -0400

    set up dynamic routing for selected cities using useParams hook

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -42,6 +43,7 @@ export default function App() {
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />{" "}
           {/* 'index' is used for Default Route */}
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,32 +1,33 @@
-import PropTypes from 'prop-types';
-import { formatDate } from '../util/formatting';
-import styles from './CityItem.module.css';
+import PropTypes from "prop-types";
+import { formatDate } from "../util/formatting";
+import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 CityItem.propTypes = {
-    city: PropTypes.shape({
-      cityName: PropTypes.string.isRequired,
-      country: PropTypes.string.isRequired,
-      emoji: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      notes: PropTypes.string,
-      position: PropTypes.shape({
-        lat: PropTypes.number.isRequired,
-        lng: PropTypes.number.isRequired,
-      }).isRequired,
-      id: PropTypes.number.isRequired,
+  city: PropTypes.shape({
+    cityName: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    emoji: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    notes: PropTypes.string,
+    position: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
     }).isRequired,
-  };
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default function CityItem({ city }) {
-    const { cityName, emoji, date } = city;   
+  const { cityName, emoji, date, id } = city;
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
-
-
