e2aa4366ce650fd8b74a38d5e7616a660a5485ac
BillStephens2022 <two4onebill@yahoo.com>
2024-07-22 09:28:50 -0400

    updated App and Sidebar with additional nested routes for cities & countries

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>LIST</p>} />  {/* 'index' is used for Default Route */}
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>List of Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
