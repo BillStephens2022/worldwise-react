dc36048145187b30b00ecab88deaf5dd52c41d83
BillStephens2022 <two4onebill@yahoo.com>
2024-08-03 06:27:13 -0400

    completed login and logout functionality

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -16,31 +16,32 @@ function reducer(state, action) {
         isAuthenticated: true,
       };
     case "logout":
-        return {
-            ...state,
-            user: null,
-            isAuthenticated: false,
-        };
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
     default:
-        throw new Error("Unrecognized action type");
+      throw new Error("Unrecognized action type");
   }
 }
 
 const FAKE_USER = {
-    name: "Jack",
-    email: "jack@example.com",
-    password: "qwerty",
-    avatar: "https://i.pravatar.cc/100?u=zz",
-  };
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
 
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
   );
+  
   function login(email, password) {
-    if(email === FAKE_USER.email && password === FAKE_USER.password) {
-        dispatch({ type: "login", payload: FAKE_USER });
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
     }
   }
 
@@ -48,7 +49,11 @@ function AuthProvider({ children }) {
     dispatch({ type: "logout" });
   }
 
-  return <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
